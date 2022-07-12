427dcd226f6e44da68f8950fed2e72abbcf3651b
MoweryOSU <MoweryOSU@gmail.com>
2022-07-12 15:10:08 -0600

    Added support for sample incr/decr functions

--- test/test_incrDecr.py
+++ test/test_incrDecr.py
@@ -1,11 +1,16 @@
-import unittest   # The test framework
+import sys
+import os
+import unittest
 
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'src'))
+
+import inc_dec
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
-        self.assertEqual(4, 4)
+        self.assertEqual(inc_dec.increment(3), 4)
 
     def test_decrement(self):
-        self.assertEqual(4, 4)
+        self.assertEqual(inc_dec.decrement(5), 4)
 
 if __name__ == '__main__':
     unittest.main()
